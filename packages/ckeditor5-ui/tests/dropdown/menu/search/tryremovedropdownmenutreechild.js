a2d56e960be061ab5be32d7ecc3baf065a2bf54c
Mateusz Baginski <cziken58@gmail.com>
2024-05-14 14:05:16 +0200

    Add testing for `tryRemoveDropdownMenuTreeChild`

---
+++ packages/ckeditor5-ui/tests/dropdown/menu/search/tryremovedropdownmenutreechild.js
@@ -0,0 +1,50 @@
+/**
+ * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+import { createTreeFromFlattenDropdownMenusList } from '../../../../src/dropdown/menu/search/createtreefromflattendropdownmenuslist.js';
+import { tryRemoveDropdownMenuTreeChild } from '../../../../src/dropdown/menu/search/tryremovedropdownmenutreechild.js';
+import { createMockDropdownMenuDefinition } from '../_utils/dropdowntreemock.js';
+
+describe( 'tryRemoveDropdownMenuTreeChild', () => {
+	it( 'should remove menu child from root node', () => {
+		const { menuRootList } = createMockDropdownMenuDefinition();
+		const tree = createTreeFromFlattenDropdownMenusList( menuRootList.menus );
+		const [ child ] = tree.children;
+
+		const resultTree = tryRemoveDropdownMenuTreeChild( tree, child );
+
+		expect( tree ).to.be.equal( resultTree );
+		expect( tree.children ).not.to.contain( child );
+	} );
+
+	it( 'should remove menu child from menu node', () => {
+		const { menuRootList } = createMockDropdownMenuDefinition();
+		const tree = createTreeFromFlattenDropdownMenusList( menuRootList.menus );
+
+		const [ parent ] = tree.children;
+		const [ child ] = parent.children;
+
+		tryRemoveDropdownMenuTreeChild( parent, child );
+		expect( parent.children ).not.to.contain( child );
+	} );
+
+	it( 'should do do nothing on item entry', () => {
+		const { menuRootList } = createMockDropdownMenuDefinition();
+		const tree = createTreeFromFlattenDropdownMenusList( menuRootList.menus );
+
+		const [ parent ] = tree.children;
+		const [ child ] = parent.children;
+
+		expect( () => {
+			tryRemoveDropdownMenuTreeChild( child, null );
+		} ).not.to.throw();
+	} );
+
+	it( 'should do do nothing on unknown entry', () => {
+		expect( () => {
+			tryRemoveDropdownMenuTreeChild( {}, null );
+		} ).not.to.throw();
+	} );
+} );
